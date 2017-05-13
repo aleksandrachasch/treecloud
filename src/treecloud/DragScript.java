cdf6380fb68128113b19bc4b7bf987dc2feae510
aleksandrachasch <chaschina.aleksandra@gmail.com>
2017-05-13 19:08:03 +0300

    add drag scripts

---
+++ src/treecloud/DragScript.java
@@ -0,0 +1,29 @@
+package treecloud;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
+import org.apache.batik.util.XMLResourceDescriptor;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.svg.SVGDocument;
+
+public class DragScript {
+	
+	public static Node getScript() throws IOException{
+	
+		String parser = XMLResourceDescriptor.getXMLParserClassName();
+		SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parser);
+		//InputStream stream = new FileInputStream("C:/Users/SONY/Documents/diploma/treecloud-ruscorpora/resources/stoplists/script.svg");
+		InputStream stream = DragScript.class.getResourceAsStream("/rectangle.svg");
+		Document doc = f.createDocument("http://www.w3.org/2000/svg", "svg" ,"http://www.w3.org/2000/svg", stream);
+		System.out.println(doc.getDocumentElement().getTagName());
+		SVGDocument svgDoc = (SVGDocument) doc;
+		System.out.println("length: " + svgDoc.getElementsByTagName("script").getLength());
+		return svgDoc.getElementsByTagName("script").item(0);
+	
+	}
+	
+}
